fd1ae14267b3b049cffade4d4abb4f52851dfec1
Neamkl <42867586+Neamkl@users.noreply.github.com>
2018-09-15 19:55:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,18 +5,11 @@ require('./util/eventLoader')(client);
 
 var prefix = ayarlar.prefix;
 
-const log = message => {
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
-};
 
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
-fs.readdir('./commands/', (err, files) => {
   if (err) console.error(err);
-  log(`${files.length} komut yüklenecek.`);
-  files.forEach(f => {
     let props = require(`./commands/${f}`);
-    log(`Yüklenen komut: ${props.help.name}.`);
     client.commands.set(props.help.name, props);
     props.conf.aliases.forEach(alias => {
       client.aliases.set(alias, props.help.name);
